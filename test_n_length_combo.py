b288f948af50bdd7bfd4361fbc3cc0a39c8fbea6
Huang Pan <huangpan@yahoo.com>
2023-10-19 11:40:50 -0700

    more testing

--- test_n_length_combo.py
+++ test_n_length_combo.py
@@ -8,18 +8,18 @@ def n_length_combo(lst, n):
     for i in range(0, len(lst)):
         m = lst[i]
         remLst = lst[i + 1:]
-        print('i m remLst', i, m, remLst)
+        print('i m remLst:', i, m, remLst)
         remainlst_combo = n_length_combo(remLst, n-1)
-        print('remainlst_combo', remainlst_combo)
+        print('remainlst_combo:', remainlst_combo)
         for p in remainlst_combo:
-            print('p', p)
-            print('(m, *p)', (m, *p))
+            print('i, m, p:', i, m, p)
+            print('(m, *p):', (m, *p))
             l.append((m, *p))
-            print('l', l)
+            print('l:', l)
 
     return l
 
 arr = ['A', 'B']
 arr = ['A', 'B', 'C']
-arr = ['A', 'B', 'C', 'D'] # [['A', 'B'], ['A', 'C'], ['A', 'D'], ['B', 'C'], ['B', 'D'], ['C', 'D']]
+#arr = ['A', 'B', 'C', 'D'] # [['A', 'B'], ['A', 'C'], ['A', 'D'], ['B', 'C'], ['B', 'D'], ['C', 'D']]
 print(n_length_combo(arr, 2))
